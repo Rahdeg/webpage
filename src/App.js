e30c30b10c5fa5e23271b3552f0176a59568deb1
Rahdeg <walett95@gmail.com>
2022-10-22 12:57:53 +0100

    added mobile menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import {Profile,Link, Footer, Social} from './components'
+import Menu from './assets/_Avatar share button.png'
 import './App.css';
 
 function App() {
   return (
     <div className="flex flex-col h-screen">
+    <div className='flex  items-center justify-end md:hidden w-full h-full'>
+    <img src={Menu} alt=''/>
+    </div>
     <Profile/>
     <Link/>
     <Social/>
