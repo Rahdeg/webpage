8e4f6758f7e0dfafa6f405fb9a184a6c9167ea2b
Rahdeg <walett95@gmail.com>
2022-10-22 08:48:23 +0100

    link details added

--- src/data/Linkdata.js
+++ src/data/Linkdata.js
@@ -1,4 +1,5 @@
 import {AiFillTwitterCircle} from 'react-icons/ai'
+import {FaGlobeAfrica} from 'react-icons/fa';
 
 
 
@@ -13,34 +14,34 @@ export const Linkdata = [
       iconSrc: <AiFillTwitterCircle className="text-textBase text-3xl cursor-pointer" />
     },
     {
-        id: "twitter__zuri",
-        name: "Twitter Link",
-        site:"https://twitter.com/Walett25",
-        iconSrc: <AiFillTwitterCircle className="text-textBase text-3xl cursor-pointer" />
+        id: "btn__zuri",
+        name: "Zuri Team",
+        site:"https://training.zuri.team/",
+        iconSrc: <FaGlobeAfrica className="text-textBase text-3xl cursor-pointer" />
       },
       {
-        id: "twitter__zuri",
-        name: "Twitter Link",
-        site:"https://twitter.com/Walett25",
-        iconSrc: <AiFillTwitterCircle className="text-textBase text-3xl cursor-pointer" />
+        id: "books",
+        name: "Zuri Books",
+        site:"http://books.zuri.team",
+        iconSrc: <FaGlobeAfrica className="text-textBase text-3xl cursor-pointer" />
       },
       {
-        id: "twitter__zuri",
-        name: "Twitter Link",
-        site:"https://twitter.com/Walett25",
-        iconSrc: <AiFillTwitterCircle className="text-textBase text-3xl cursor-pointer" />
+        id: "book__python",
+        name: "Python Books",
+        site:"https://books.zuri.team/python-for-beginners?ref_id=rahdeg",
+        iconSrc: <FaGlobeAfrica className="text-textBase text-3xl cursor-pointer" />
       },
       {
-        id: "twitter__zuri",
-        name: "Twitter Link",
-        site:"https://twitter.com/Walett25",
-        iconSrc: <AiFillTwitterCircle className="text-textBase text-3xl cursor-pointer" />
+        id: "pitch",
+        name: "Background Check for Coders",
+        site:" https://background.zuri.team",
+        iconSrc: <FaGlobeAfrica className="text-textBase text-3xl cursor-pointer" />
       },
       {
-        id: "twitter__zuri",
-        name: "Twitter Link",
-        site:"https://twitter.com/Walett25",
-        iconSrc: <AiFillTwitterCircle className="text-textBase text-3xl cursor-pointer" />
+        id: "book__design",
+        name: "Design Books",
+        site:"https://books.zuri.team/design-rules",
+        iconSrc: <FaGlobeAfrica className="text-textBase text-3xl cursor-pointer" />
       },
     
   ];
