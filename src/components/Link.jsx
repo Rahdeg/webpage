3b011630876c157d242395b8a7c3a78c15b49bc2
Rahdeg <walett95@gmail.com>
2022-10-28 01:14:17 +0100

    text

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -7,7 +7,7 @@ const Link = () => {
     {
         Linkdata && Linkdata.map((data,idx)=>(
           <a href={data.site} target="_blank" rel="noreferrer" key={idx} id={data.id}
-  className=" bg-gray-400 flex items-center justify-center hover:bg-gray-500 text-white py-2 px-4 border border-gray-500 hover:border-transparent rounded w-2/3">{data.name}</a>
+  className=" bg-gray-400 flex items-center justify-center hover:bg-gray-500 text-black py-2 px-4 border border-gray-500 hover:border-transparent rounded w-2/3">{data.name}</a>
            
         ))
     }
