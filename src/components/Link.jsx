244f3c6fce52728022e5db35b4a4dad823aff2f6
Rahdeg <walett95@gmail.com>
2022-10-28 01:08:34 +0100

    id added

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -6,7 +6,7 @@ const Link = () => {
     <div className='flex items-center justify-center flex-col gap-5'>
     {
         Linkdata && Linkdata.map((data,idx)=>(
-          <a href={data.site} target="_blank" rel="noreferrer" key={idx}
+          <a href={data.site} target="_blank" rel="noreferrer" key={idx} id={data.id}
   className=" bg-gray-400 flex items-center justify-center hover:bg-gray-500 text-white py-2 px-4 border border-gray-500 hover:border-transparent rounded w-2/3">{data.name}</a>
            
         ))
