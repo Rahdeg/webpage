de87779bd30b4841ffd6e215f27f4b5aafdbfe67
Rahdeg <walett95@gmail.com>
2022-11-05 11:18:41 +0100

    fix: fixed bug

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Image from '../assets/profile.png'
-import Menu from '../assets/_Avatar share button (1).png'
+// import Menu from '../assets/_Avatar share button (1).png'
 
 const Profile = () => {
   return (
     <div className='flex flex-col items-center mb-8 '>
-    <div className='hidden md:flex  md:items-center md:justify-evenly md:ml-96  md:relative w-full h-full'>
+    {/*<div className='hidden md:flex  md:items-center md:justify-evenly md:ml-96  md:relative w-full h-full'>
     <img src={Menu} alt=''/>
-    </div>
+  </div>*/}
     <div class="relative">
     <img class="w-10 h-10 rounded-full" src={Image} alt="" id='profile__img'/>
     <span class="top-0 left-7 absolute  w-3.5 h-3.5 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full"></span>
