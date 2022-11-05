73bdbb1438595946d0805312199e98f6574fa2ed
Rahdeg <walett95@gmail.com>
2022-11-05 23:01:45 +0100

    fix: fixed bug

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,29 +1,12 @@
 import React from "react";
 import { Linkdata } from "../data/Linkdata";
-import { Link } from "react-router-dom";
 
 const Links = () => {
   return (
     <div className="flex items-center justify-center flex-col gap-5">
       {Linkdata &&
         Linkdata.map((data, idx) =>
-          data.id === "contact" ? (
-            <Link
-              to={data.site}
-              className=" bg-cartBg flex items-center justify-center hover:bg-gray-200 text-black  border border-gray-200 hover:border-transparent rounded w-2/3"
-            >
-              <a
-                href="#0"
-                target="_blank"
-                rel="noreferrer"
-                key={idx}
-                id={data.id}
-                className=" bg-cartBg flex items-center justify-center hover:bg-gray-200 text-black py-2 px-4 border border-gray-200 hover:border-transparent rounded w-2/3"
-              >
-                {data.name}
-              </a>
-            </Link>
-          ) : (
+          (
             <a
               href={data.site}
               target="_blank"
