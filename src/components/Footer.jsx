ee6f9546aa2f56cf952c31297d3439f676a9e005
Rahdeg <walett95@gmail.com>
2022-11-05 11:12:32 +0100

    fix: fix bug

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import Zuri from '../assets/I4G.png'
 
 const Footer = () => {
   return (
-    <div className=' flex flex-col md:flex-row items-center justify-around mt-10 p-4 mb-5 border-t-2 '>
+    <div className=' flex flex-col justify-between md:flex-row items-start  mt-10 p-4 mb-5 border-t-2 '>
     <h2 className='text-primary font-bold'>Zuri <span className=' text-red-600'>.</span>Internship</h2>
     <p className=' text-gray-400'>HNG Internship 9 Frontend Task</p>
     <img src={Zuri} alt='' className=''/>
--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -13,7 +13,7 @@ const Links = () => {
 </Link>
            :
     <a href={data.site} target="_blank" rel="noreferrer" key={idx} id={data.id}
-className=" bg-cartBg flex items-center justify-center hover:bg-gray-300 text-black py-2 px-4 border border-gray-200 hover:border-transparent rounded w-2/3">{data.name}</a>
+className=" bg-cartBg flex text-sm md:text-base items-center justify-center hover:bg-gray-300 text-black py-2 px-4 border border-gray-200 hover:border-transparent rounded w-2/3">{data.name}</a>
      
   ))
            
